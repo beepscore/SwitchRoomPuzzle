48689f37e6b21e623beb52cb0af7f3fb2643be0a
beepscore <support@beepscore.com>
2017-07-29 13:37:43 -0700

    Add PrisonerTests

---
+++ SwitchRoomPuzzleTests/PrisonerTests.swift
@@ -0,0 +1,19 @@
+//
+//  PrisonerTests.swift
+//  SwitchRoomPuzzle
+//
+//  Created by Steve Baker on 7/29/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import XCTest
+@testable import SwitchRoomPuzzle
+
+class PrisonerTests: XCTestCase {
+    
+    func testInit() {
+        let prisoner = Prisoner()
+        XCTAssertNotNil(prisoner)
+    }
+    
+}
