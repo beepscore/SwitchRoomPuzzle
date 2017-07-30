f759903e57689b7f7bf9a7951aa7061c899978f5
beepscore <support@beepscore.com>
2017-07-29 18:05:32 -0700

    Add assertions

--- SwitchRoomPuzzleTests/PrisonerTests.swift
+++ SwitchRoomPuzzleTests/PrisonerTests.swift
@@ -14,11 +14,14 @@ class PrisonerTests: XCTestCase {
     func testInit() {
         let prisoner = Prisoner()
         XCTAssertNotNil(prisoner)
+        XCTAssertFalse(prisoner.didVisitRoomAtLeastOnce)
     }
 
     func testVisitRoomAndReport() {
         let prisoner = Prisoner()
+        XCTAssertFalse(prisoner.didVisitRoomAtLeastOnce)
         XCTAssertTrue(prisoner.visitRoomAndReport(Room()))
+        XCTAssertTrue(prisoner.didVisitRoomAtLeastOnce)
     }
 
 }
